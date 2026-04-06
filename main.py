8bb08d7ca3ca4ed93a88cf4b678e4c604b2350fb
Wind <hengweiyi2@gmail.com>
2026-04-06 13:08:44 +0800

    Change

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ y = data['label']
 
 # Split ONCE
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2, random_state=42
+    x, y, test_size=0.2, random_state=1
 )
 
 # Run models
